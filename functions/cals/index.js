b2408a4e2657bca7b09557e7e3ed79401ce4cf47
javahariprasad <javahariprasad@gmail.com>
2022-09-22 17:26:54 +0530

    add

--- functions/cals/index.js
+++ functions/cals/index.js
@@ -28,6 +28,7 @@
 
     }
     DataApi dataApi = context.getOrg().get().getDataApi(); 
+    
     UnitOfWorkBuilder uow = dataApi.newUnitOfWorkBuilder();
 
     // Define a record using the RecordForCreate type and providing the Developer Name
